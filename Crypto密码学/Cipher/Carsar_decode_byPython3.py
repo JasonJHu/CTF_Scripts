8ad81236c47de1d7ce2c07f42e108c26a79c5472
JasonJHu <44105777+JasonJHu@users.noreply.github.com>
2022-07-02 22:45:22 +0800

    Add files via upload

---
+++ Crypto密码学/Cipher/Carsar_decode_byPython3.py
@@ -0,0 +1,65 @@
+#-*- coding: UTF-8 -*- 
+# Pythen 3.8
+# Caesar Cipher
+
+MAX_KEY_SIZE = 26
+
+def getMode():
+    while True:
+        print('请选择加密或解密模式,或者选择暴力破解：')
+        print('加密:encrypt(e)')
+        print('解密:decrypt(d)')
+        print('暴力破解:brute(b)')
+        mode = input().lower()
+        if mode in 'encrypt e decrypt d brute b'.split():
+            return mode
+        else:
+            print('请输入"encrypt"或"e"或"decrypt"或"d"或"brute"或"b"!')
+
+def getMessage():
+    print('请输入你的信息：')
+    return input()
+
+def getKey():
+    key = 0
+    while True:
+        print('请输入密钥数字(1-%s)' % (MAX_KEY_SIZE))
+        key = int(input())
+        if (key >=1 and key <= MAX_KEY_SIZE):
+            return key
+
+def getTranslatedMessage(mode, message, key):
+    if mode[0] == 'd':
+        key = -key
+    translated = ''
+    for symbol in message:
+        if symbol.isalpha():
+            num = ord(symbol)
+            num += key
+            if symbol.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif symbol.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
+
+            translated += chr(num)
+        else:
+            translated += symbol
+    return translated
+
+mode = getMode()
+message = getMessage()
+if mode[0] != 'b':
+    key = getKey()
+
+print('你要翻译的信息是:')
+if mode[0] != 'b':
+    print(getTranslatedMessage(mode, message, key))
+else:
+    for key in range(1, MAX_KEY_SIZE + 1):
+        print(key, getTranslatedMessage('decrypt', message, key))
