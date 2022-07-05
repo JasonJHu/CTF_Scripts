a6179a7bbfc5885f1e1817a683c7d5f2d147b2b9
JasonJHu <44105777+JasonJHu@users.noreply.github.com>
2022-07-05 21:01:13 +0800

    Create Carsar_plus_byPython3.py

---
+++ Crypto密码学/Cipher/Carsar_plus_byPython3.py
@@ -0,0 +1,24 @@
+#-*- coding: UTF-8 -*- 
+# Pythen 3.8
+
+# 密文afZ_r9VYfScOeO_UL^RWUc
+# 明文flag{...}
+# 等差变化 5/6/7/8/9/...
+
+def carsar_decode(sCiper):
+    j = 5
+    i = 0
+    sReturn = []
+    for i in range(len(sCiper)):
+        m = ord(sCiper[i])          # 将密文的第i个字母变为其ascii码值
+        m = m + j                 # ascii值+j
+        sReturn.append(chr(m))           # 将递进后的ascii值存入列表sReturn[]
+        j += 1
+    return ''.join(sReturn)
+
+
+if __name__ == '__main__':
+    sCiper = 'afZ_r9VYfScOeO_UL^RWUc'    # 密文
+    sMessage = []
+    sMessage = carsar_decode(sCiper)
+    print(sMessage)
